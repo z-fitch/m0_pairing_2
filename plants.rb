ac6a4e851fa3d2b20d1028605b9a2c52756a6f2e
Zanna Fitch <zannafitch2004@gmail.com>
2023-03-29 19:37:53 -0500

    Initial Commit

---
+++ plants.rb
@@ -0,0 +1,8 @@
+gets_sunshine = true
+ounces_of_water = 11
+
+if gets_sunshine == true && ounces_of_water >= 10
+    puts "THis will Live!!"
+else gets_sunshine != true || ounces_of_water < 10
+    puts "This plant will not survive :("
+end
